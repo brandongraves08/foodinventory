98c39b0448508574e675fe193093a1d7690c9d23
Brandon Graves <brandongraves08@gmail.com>
2025-06-10 12:37:42 -0500

    Add User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,18 +1,22 @@
-from sqlalchemy import Boolean, Column, String
+from typing import TYPE_CHECKING
+import uuid
+from sqlalchemy import Column, String, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
-import uuid
 
 from app.db.base_class import Base
+
+if TYPE_CHECKING:
+    from .food_item import FoodItem  # noqa: F401
 
 
 class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = Column(String, unique=True, index=True, nullable=False)
-    username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    full_name = Column(String, index=True)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
     
-    # Relationship with food items
-    food_items = relationship("FoodItem", back_populates="owner")
+    # Relationships
+    food_items = relationship("FoodItem", back_populates="owner", cascade="all, delete-orphan")
