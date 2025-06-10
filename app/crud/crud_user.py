cda2c6f58cf60ad684d1632c20cd425242bf6e9f
Brandon Graves <brandongraves08@gmail.com>
2025-06-10 12:39:05 -0500

    Add User CRUD operations

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, Optional, Union
+from uuid import UUID
 
 from sqlalchemy.orm import Session
 
@@ -12,14 +13,11 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
         return db.query(User).filter(User.email == email).first()
 
-    def get_by_username(self, db: Session, *, username: str) -> Optional[User]:
-        return db.query(User).filter(User.username == username).first()
-
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
         db_obj = User(
             email=obj_in.email,
-            username=obj_in.username,
             hashed_password=get_password_hash(obj_in.password),
+            full_name=obj_in.full_name,
             is_superuser=obj_in.is_superuser,
         )
         db.add(db_obj)
