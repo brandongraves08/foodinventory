392be902ab0b881b119ac3ed47586568611f4176
Brandon Graves <brandongraves08@gmail.com>
2025-06-10 12:38:11 -0500

    Add security utilities for authentication

--- app/core/security.py
+++ app/core/security.py
@@ -9,9 +9,6 @@ from app.core.config import settings
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-DEFAULT_ALGORITHM = "HS256"
-
-
 def create_access_token(
     subject: Union[str, Any], expires_delta: timedelta = None
 ) -> str:
@@ -22,7 +19,7 @@ def create_access_token(
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
     to_encode = {"exp": expire, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=DEFAULT_ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 
