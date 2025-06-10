32ede2e16961d8b691791fa3543178402b3d6ae4
Brandon Graves <brandongraves08@gmail.com>
2025-06-10 12:21:53 -0500

    Update user model to include food items relationship

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Boolean, Column, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 import uuid
 
 from app.db.base_class import Base
@@ -12,3 +13,6 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
+    
+    # Relationship with food items
+    food_items = relationship("FoodItem", back_populates="owner")
